ce03c1057d1702129e72342589203eae8a028c56
yunatainaka <yuna.tainaka@gmail.com>
2024-12-15 16:46:19 +0900

    [fix]リファクタリング

--- run.py
+++ run.py
@@ -1,35 +1,10 @@
-import glob
 import os
 
-
-import os, sys
+import pathlib
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
-from tkinter import filedialog
-
-
-# def file_rename():
-#     files = glob.glob("./B版/*")
-#     for file in files:
-#         rename = (
-#             file.replace("_(", "_")
-#             .replace(")", "_")
-#             .replace(".01", "")
-#             .replace("step2.0", "step2.0)")
-#             .replace("__", "_")
-#         )
-#         print(rename)
-#         # os.rename(file, rename)
-
-
-# def file_version_update():
-#     files = glob.glob("./B版/*")
-#     for file in files:
-#         rename = file.replace("_A_", "_C_").replace("_B_", "_C_")
-#         print(rename)
-#         os.rename(file, rename)
-
+import shutil
 
 class FileRename:
     def __init__(self):
@@ -38,7 +13,6 @@ class FileRename:
         self.output_folder = ""
 
     def main(self):
-        print("AAAAAA")
         self._create_GUI()
 
     def _create_GUI(self):
@@ -57,10 +31,19 @@ class FileRename:
         ３．前方一致検索、部分一致検索、後方一致検索を選択する。
             """,
         ).place(x=0, y=0)
+        tk.Message(
+            root,
+            width=400,
+            text="""※注意
+        ・置換後文字列が空白の場合は、置換前文字列を削除する
+        ・出力フォルダが未選択の場合は入力フォルダ内のファイルを上書きする
+        ・置換後のファイル名が重複する場合、該当ファイルの置換は行わない（未作成）
+            """,
+        ).place(x=0, y=100)
 
         # 入力フォルダフレーム
         frame1 = ttk.Frame(root, width=360, height=60, padding=10, relief="groove")
-        frame1.place(x=20, y=100)
+        frame1.place(x=20, y=200)
         tk.Label(frame1, text="入力フォルダを選択してください").place(x=0, y=0)
         input_path = tk.StringVar()
         IDirEntry = ttk.Entry(
@@ -75,7 +58,7 @@ class FileRename:
 
         # 置換文言フレーム
         frame2 = ttk.Frame(root, width=360, height=60, padding=10, relief="groove")
-        frame2.place(x=20, y=160)
+        frame2.place(x=20, y=260)
 
         tk.Label(frame2, text="置換する文言を入力してください").place(x=0, y=0)
         self.before = tk.StringVar()
@@ -86,33 +69,17 @@ class FileRename:
         IDirEntry = ttk.Entry(frame2, textvariable=self.after, width=20)
         IDirEntry.place(x=150, y=20)
 
-        # ラジオボタンフレーム
-        frame3 = ttk.Frame(root, width=360, height=100, padding=10, relief="groove")
-        frame3.place(x=20, y=220)
-        tk.Label(frame3, text="検索種別を選択してください").place(x=0, y=0)
-        self.selected_option = tk.StringVar(value=1)
-        tk.Radiobutton(
-            frame3, text="前方一致", variable=self.selected_option, value="1"
-        ).place(x=0, y=20)
-        tk.Radiobutton(
-            frame3, text="部分一致", variable=self.selected_option, value="2"
-        ).place(x=0, y=40)
-        tk.Radiobutton(
-            frame3, text="後方一致", variable=self.selected_option, value="3"
-        ).place(x=0, y=60)
-        # tk.Button(text=("終了"), command=quit).place(x=150, y=150)
-
         # 出力フォルダフレーム
-        frame4 = ttk.Frame(root, width=360, height=60, padding=10, relief="groove")
-        frame4.place(x=20, y=320)
-        tk.Label(frame4, text="出力フォルダを選択してください").place(x=0, y=0)
+        frame3 = ttk.Frame(root, width=360, height=60, padding=10, relief="groove")
+        frame3.place(x=20, y=320)
+        tk.Label(frame3, text="出力フォルダを選択してください").place(x=0, y=0)
         output_path = tk.StringVar()
         IDirEntry = ttk.Entry(
-            frame4, textvariable=output_path, width=48, state="readonly"
+            frame3, textvariable=output_path, width=48, state="readonly"
         )
         IDirEntry.place(x=0, y=20)
         tk.Button(
-            frame4,
+            frame3,
             text=("選択"),
             command=lambda: self._push_output_folder_bottun(output_path),
         ).place(x=300, y=18)
@@ -141,26 +108,32 @@ class FileRename:
             error_list.append("入力フォルダを選択してください")
         if self.before.get() == "":
             error_list.append("置換前文言を入力してください")
-        if self.after.get() == "":
-            error_list.append("置換後文言を入力してください")
 
         return error_list
+    
+    def _error_dialog(self, error_list):
+        error_message = '\n'.join(error_list)
+        messagebox.showerror("エラー",error_message)
 
     def _file_rename(self):
         error_list = self._check()
         if len(error_list) != 0:
-            print(error_list)
+            self._error_dialog(error_list)
             return
+        
+          
         print("input_folder: ", self.input_folder)
         print("output_folder: ", self.output_folder)
-        print("before: ", self.before.get())
-        print("after: ", self.after.get())
-        print("selected_option:", self.selected_option.get())
-        files = glob.glob(f"{self.input_folder}/*")
-        for file in files:
-            rename = file.replace(self.before.get(), self.after.get())
-            print(file)
-            os.rename(file, rename)
+
+        print("self.before.get(), self.after.get()",self.before.get(), self.after.get())
+        p_temp = pathlib.Path(self.input_folder).glob('*.txt')
+        for p in p_temp:
+            if self.output_folder == "":
+                rename = p.name.replace(self.before.get(), self.after.get())
+                os.rename(f"{self.input_folder}/{p.name}", f"{self.input_folder}/{rename}")
+            else:                
+                rename = p.name.replace(self.before.get(), self.after.get())
+                shutil.copy2(f"{self.input_folder}/{p.name}", f"{self.output_folder}/{rename}")
 
 
 if __name__ == "__main__":
