dde02da0061e00a1bb1549a64efcc11f0ddcbebf
tuna <yuna.tainaka@gmail.com>
2025-01-26 10:32:08 +0900

    [fix]不具合修正、見た目変更、コード成形

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ from tkinter import filedialog
 from tkinter import simpledialog
 import shutil
 
+
 class FileRename:
     def __init__(self):
         self.input_folder = ""
@@ -19,19 +20,8 @@ class FileRename:
     def _create_GUI(self):
         root = tk.Tk()
         root.title("File Rename Tool")
-        root.geometry("400x400")
         root.resizable(False, False)
 
-        tk.Message(
-            root,
-            width=400,
-            text="""～使い方～
-        １．フォルダを選択する。
-        　　選択したフォルダ内の全てのファイルを対象としてファイル名の置換を行う。
-        ２．置換対象のファイル名を入力する
-        ３．前方一致検索、部分一致検索、後方一致検索を選択する。
-            """,
-        ).place(x=0, y=0)
         tk.Message(
             root,
             width=400,
@@ -39,17 +29,17 @@ class FileRename:
         ・置換後文字列が空白の場合は、置換前文字列を削除する
         ・出力フォルダが未選択の場合は入力フォルダ内のファイルを上書きする
         """,
-        ).place(x=0, y=100)
+        ).pack()
 
         # 入力フォルダフレーム
         frame1 = ttk.Frame(root, width=360, height=60, padding=10, relief="groove")
-        frame1.place(x=20, y=160)
+        frame1.pack()
         tk.Label(frame1, text="入力フォルダを選択してください").place(x=0, y=0)
         input_path = tk.StringVar()
-        IDirEntry = ttk.Entry(
+        input_folder = ttk.Entry(
             frame1, textvariable=input_path, width=48, state="readonly"
         )
-        IDirEntry.place(x=0, y=20)
+        input_folder.place(x=0, y=20)
         tk.Button(
             frame1,
             text=("選択"),
@@ -58,26 +48,25 @@ class FileRename:
 
         # 置換文言フレーム
         frame2 = ttk.Frame(root, width=360, height=60, padding=10, relief="groove")
-        frame2.place(x=20, y=220)
-
+        frame2.pack()
         tk.Label(frame2, text="置換する文言を入力してください").place(x=0, y=0)
         self.before = tk.StringVar()
-        IDirEntry = ttk.Entry(frame2, textvariable=self.before, width=20)
-        IDirEntry.place(x=0, y=20)
+        before_word = ttk.Entry(frame2, textvariable=self.before, width=20)
+        before_word.place(x=0, y=20)
         tk.Label(frame2, text="→").place(x=130, y=20)
         self.after = tk.StringVar()
-        IDirEntry = ttk.Entry(frame2, textvariable=self.after, width=20)
-        IDirEntry.place(x=150, y=20)
+        after_word = ttk.Entry(frame2, textvariable=self.after, width=20)
+        after_word.place(x=150, y=20)
 
         # 出力フォルダフレーム
         frame3 = ttk.Frame(root, width=360, height=60, padding=10, relief="groove")
-        frame3.place(x=20, y=280)
+        frame3.pack()
         tk.Label(frame3, text="出力フォルダを選択してください").place(x=0, y=0)
         output_path = tk.StringVar()
-        IDirEntry = ttk.Entry(
+        output_folder = ttk.Entry(
             frame3, textvariable=output_path, width=48, state="readonly"
         )
-        IDirEntry.place(x=0, y=20)
+        output_folder.place(x=0, y=20)
         tk.Button(
             frame3,
             text=("選択"),
@@ -88,7 +77,7 @@ class FileRename:
             root,
             text=("置換開始"),
             command=lambda: self._file_rename(),
-        ).place(x=180, y=350)
+        ).pack(pady=10)
 
         root.mainloop()
 
@@ -110,10 +99,10 @@ class FileRename:
             error_list.append("置換前文言を入力してください")
 
         return error_list
-    
+
     def _error_dialog(self, error_list):
-        error_message = '\n'.join(error_list)
-        messagebox.showerror("エラー",error_message)
+        error_message = "\n".join(error_list)
+        messagebox.showerror("エラー", error_message)
 
     def _file_rename(self):
         error_list = self._check()
@@ -121,8 +110,7 @@ class FileRename:
             self._error_dialog(error_list)
             return
 
-        p_temp = pathlib.Path(self.input_folder).glob('*.txt')
-
+        p_temp = pathlib.Path(self.input_folder).glob("*.*")
 
         cnt = 0
         for p in p_temp:
@@ -130,27 +118,41 @@ class FileRename:
                 continue
 
             rename = p.name.replace(self.before.get(), self.after.get())
-            if self.output_folder == "":      
+            if self.output_folder == "":
                 if self._check_renamed_file(f"{self.input_folder}/{rename}"):
-                    cnt +=1
+                    cnt += 1
                     try:
-                        os.rename(f"{self.input_folder}/{p.name}", f"{self.input_folder}/{rename}")
+                        os.rename(
+                            f"{self.input_folder}/{p.name}",
+                            f"{self.input_folder}/{rename}",
+                        )
                     except:
                         os.remove(f"{self.input_folder}/{rename}")
-                        os.rename(f"{self.input_folder}/{p.name}", f"{self.input_folder}/{rename}")
+                        os.rename(
+                            f"{self.input_folder}/{p.name}",
+                            f"{self.input_folder}/{rename}",
+                        )
             else:
                 if self._check_renamed_file(f"{self.output_folder}/{rename}"):
-                    cnt +=1
-                    shutil.copy2(f"{self.input_folder}/{p.name}", f"{self.output_folder}/{rename}")
-        
+                    cnt += 1
+                    shutil.copy2(
+                        f"{self.input_folder}/{p.name}",
+                        f"{self.output_folder}/{rename}",
+                    )
+
         messagebox.showinfo("置換ファイル数", f"{cnt}件のファイルを置換しました")
 
-    def _check_renamed_file(self, output_path):        
+    def _check_renamed_file(self, output_path):
         if os.path.isfile(output_path):
-            res = messagebox.askokcancel("上書き確認", f"{output_path}は存在します。\nファイルの上書きをしますか？")
+            res = messagebox.askokcancel(
+                "上書き確認",
+                f"{output_path}は存在します。\nファイルの上書きをしますか？",
+            )
             return res
         else:
             return True
+
+
 if __name__ == "__main__":
     fr = FileRename()
     fr.main()
